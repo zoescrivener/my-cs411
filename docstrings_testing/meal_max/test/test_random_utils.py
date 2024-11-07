45d51d9dd402f7ca6951c64a8ba63ca95870111a
Nicholas Zhang <nicholas@crc-dot1x-nat-10-239-128-17.bu.edu>
2024-11-07 16:20:34 -0500

    added unit test for random_utils

--- docstrings_testing/meal_max/test/test_random_utils.py
+++ docstrings_testing/meal_max/test/test_random_utils.py
@@ -1,11 +1,11 @@
 import pytest
 import requests
 
-from music_collection.utils.random_utils import get_random
+from meal_max.utils.random_utils import get_random
 
 
 RANDOM_NUMBER = 42
-NUM_SONGS = 100
+NUM_MEALS = 100
 
 @pytest.fixture
 def mock_random_org(mocker):
@@ -20,7 +20,7 @@ def mock_random_org(mocker):
 
 def test_get_random(mock_random_org):
     """Test retrieving a random number from random.org."""
-    result = get_random(NUM_SONGS)
+    result = get_random(NUM_MEALS)
 
     # Assert that the result is the mocked random number
     assert result == RANDOM_NUMBER, f"Expected random number {RANDOM_NUMBER}, but got {result}"
@@ -33,18 +33,18 @@ def test_get_random_request_failure(mocker):
     mocker.patch("requests.get", side_effect=requests.exceptions.RequestException("Connection error"))
 
     with pytest.raises(RuntimeError, match="Request to random.org failed: Connection error"):
-        get_random(NUM_SONGS)
+        get_random(NUM_MEALS)
 
 def test_get_random_timeout(mocker):
     """Simulate  a timeout."""
     mocker.patch("requests.get", side_effect=requests.exceptions.Timeout)
 
     with pytest.raises(RuntimeError, match="Request to random.org timed out."):
-        get_random(NUM_SONGS)
+        get_random(NUM_MEALS)
 
 def test_get_random_invalid_response(mock_random_org):
     """Simulate  an invalid response (non-digit)."""
     mock_random_org.text = "invalid_response"
 
     with pytest.raises(ValueError, match="Invalid response from random.org: invalid_response"):
-        get_random(NUM_SONGS)
+        get_random(NUM_MEALS)
