b9a4310d508766eb28f2834ee8ce6db750b438be
Nicholas Zhang <nicholas@crc-dot1x-nat-10-239-128-17.bu.edu>
2024-11-08 02:23:30 -0500

    added all unit tests for battle_model except for the battle function

--- docstrings_testing/meal_max/test/test_battle_model.py
+++ docstrings_testing/meal_max/test/test_battle_model.py
@@ -22,6 +22,8 @@ def sample_meal_1():
     return Meal("Spaghetti", "Italian", 10.0, "LOW")
 def sample_meal_2():
     return Meal("Pizza", "Italian", 15.0, "MEDIUM")
+def sample_meal_3():
+    return Meal("Sushi", "Japanese", 20.0, "HIGH")
 
 @pytest.fixture
 def sample_conbatants(sample_meal_1, sample_meal_2):
@@ -34,11 +36,45 @@ def sample_conbatants(sample_meal_1, sample_meal_2):
 
 def test_prep_combatants(battle_model, sample_combatants):
     """Test adding combatants to the battle model."""
-    battle_model.prep_combatants(sample_combatants[0])
-    battle_model.prep_combatants(sample_combatants[1])
+    battle_model.prep_combatants(sample_meal_1)
+    battle_model.prep_combatants(sample_meal_2)
 
     assert len(battle_model.combatants) == 2
     assert battle_model.combatants == sample_combatants
 
-def test_add_duplicate_combatants(battle_model, sample_combatants):
-    """test adding t"""
+def test_add_extra_combatants(battle_model, sample_combatants):
+    """ Test adding extra combatants to combatants."""
+    battle_model.combatants = sample_combatants
+    with pytest.raises(ValueError, match="Combatant list is full, cannot add more combatants."):
+        battle_model.prep_combatants(sample_meal_3)
+
+
+def test_clear_combatants(battle_model, sample_combatants):
+    """Test clearing combatants from the battle model."""
+    battle_model.combatants = sample_combatants
+    battle_model.clear_combatants()
+
+    assert len(battle_model.combatants) == 0
+    assert battle_model.combatants == []
+
+def test_get_combatants(battle_model, sample_combatants):
+    """Test returning combatants from the battle model."""
+    battle_model.combatants = sample_combatants
+
+    assert battle_model.get_combatants() == sample_combatants
+
+def test_get_battle_score(battle_model, sample_meal_1):
+    """Test calculating battle score of a meal."""
+    score = battle_model.get_battle_score(sample_meal_1)
+
+    assert score == 67.0
+
+def test_battle(battle_model, sample_combatants):
+    """Test battling two combatants."""
+    battle_model.combatants = sample_combatants
+    battle_result = battle_model.battle()
+
+    assert battle_result == sample_combatants[1]
+
+    """need to test for , removing the loser,
+    updating the stats for each combatant, all the intermediate calculations"""
